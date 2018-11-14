5d52fb4cc282de9d4c702feb090ddcf8661c92fa
Max Nesterov <menesterov@gmail.com>
2018-11-14 11:40:10 +0200

    comments

--- src/Transformer/TextTransformer.php
+++ src/Transformer/TextTransformer.php
@@ -2,11 +2,24 @@
 
 namespace  Transformer;
 
+
+/**
+* Transform text
+* @package Transformer
+* @version 1.0
+* @author Nesterov Max <menesterov@gmail.com>
+*/
 class TextTransformer {
 
 	const OPTION_METHOD = ['trim', 'singleSpace'];
 	private $text = '';
 
+	/**
+	* Create TextTransformer for text
+	* @param string $text Initial text
+	* @param array $options Methods for initial text transformation
+	* @return TextTransformer
+	*/
 	function __construct($text, $options = []) {
 
 		$this->text = strval($text);
@@ -18,20 +31,37 @@ class TextTransformer {
 		}
 	}
 
+
+	/**
+	* Get current text
+	* @return string text
+	*/
 	public function get() {
 		return $this->text;
 	}
 
+	/**
+	* Trim spaces in current text
+	* @return TextTransformer (self)
+	*/
 	public function trim() {
 		$this->text = trim($this->text);
 		return $this;
 	}
 
+	/**
+	* Fix double spaces to mono space inside current text
+	* @return TextTransformer (self)
+	*/
 	public function singleSpace() {
 		$this->text = preg_replace("/\s{2,}/", ' ', $this->text);
 		return $this;
 	}
 
+	/**
+	* Sort chars in every word for current text
+	* @return TextTransformer (self)
+	*/
 	public function sortCharsInWords() {
 		$words = mb_split('\s', $this->text);
 		foreach ($words as &$word) {
@@ -41,6 +71,11 @@ class TextTransformer {
 		return $this;
 	}
 
+	/**
+	* Sort chars in every word for current text
+	* @param string $word A unit word
+	* @return string Word with sorted chars 
+	*/
 	private function sortChars($word) {
 		$chars = preg_split('//u', $word, null, PREG_SPLIT_NO_EMPTY);
 		natsort($chars);
